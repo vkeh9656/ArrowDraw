957bd1a1dd071c1556c684734afcc12f7dad791a
DESKTOP-9FFSOO7\tjdwn <tjdwns9656@naver.com>
2022-12-14 07:20:03 +0900

    라인 생성

--- ArrowDrawDlg.cpp
+++ ArrowDrawDlg.cpp
@@ -31,6 +31,10 @@ void CArrowDrawDlg::DoDataExchange(CDataExchange* pDX)
 BEGIN_MESSAGE_MAP(CArrowDrawDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_WM_DESTROY()
+	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONUP()
+	ON_WM_MOUSEMOVE()
 END_MESSAGE_MAP()
 
 
@@ -45,7 +49,15 @@ BOOL CArrowDrawDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
 	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
 
-	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+	GetClientRect(m_rect); // 현재 클라이언트 좌표계를 m_rect에 저장
+
+	m_image.Create(m_rect.Width(), m_rect.Height(), 32);
+	m_image_dc.Attach(m_image.GetDC());
+
+	m_green_pen.CreatePen(PS_SOLID, 1, RGB(100, 255, 100));
+	m_green_brush.CreateSolidBrush(RGB(0, 160, 0));
+
+	SetBackgroundColor(RGB(0, 0, 0));
 
 	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
 }
@@ -56,10 +68,9 @@ BOOL CArrowDrawDlg::OnInitDialog()
 
 void CArrowDrawDlg::OnPaint()
 {
+	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
 	if (IsIconic())
 	{
-		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
-
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
 		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
@@ -75,7 +86,8 @@ void CArrowDrawDlg::OnPaint()
 	}
 	else
 	{
-		CDialogEx::OnPaint();
+		m_image.Draw(dc, 0, 0);
+		//CDialogEx::OnPaint();
 	}
 }
 
@@ -86,3 +98,49 @@ HCURSOR CArrowDrawDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CArrowDrawDlg::OnDestroy()
+{
+	CDialogEx::OnDestroy();
+
+	m_image_dc.Detach();
+	m_image.ReleaseDC();
+	
+	m_green_pen.DeleteObject();
+	m_green_brush.DeleteObject();
+}
+
+
+void CArrowDrawDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	m_is_clicked = 1;
+	m_start_pos = point;
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CArrowDrawDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	m_is_clicked = 0;
+
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
+
+
+void CArrowDrawDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	if (m_is_clicked == 1) // 클릭한 상태에서 드래그한 경우
+	{
+		m_image_dc.FillSolidRect(m_rect, RGB(0, 0, 0)); // 잔상 초기화
+
+		m_image_dc.SelectObject(&m_green_pen);
+		m_image_dc.MoveTo(m_start_pos);
+		m_image_dc.LineTo(point);
+		
+		Invalidate(FALSE);
+	}
+
+	CDialogEx::OnMouseMove(nFlags, point);
+}
--- ArrowDrawDlg.h
+++ ArrowDrawDlg.h
@@ -8,6 +8,17 @@
 // CArrowDrawDlg 대화 상자
 class CArrowDrawDlg : public CDialogEx
 {
+private:
+	CImage m_image;
+	CDC m_image_dc;
+
+	CPen m_green_pen;
+	CBrush m_green_brush;
+
+	CRect m_rect;
+	char m_is_clicked = 0; // 0 : non-click,   1: click
+	CPoint m_start_pos;
+
 // 생성입니다.
 public:
 	CArrowDrawDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
@@ -30,4 +41,9 @@ protected:
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnDestroy();
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 };
